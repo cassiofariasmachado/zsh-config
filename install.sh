3ef2123f1c21134a71e45f4a57d2946a5b09ce11
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2022-08-29 21:31:14 -0300

    🐛 corrigir script de instalação

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 echo "✨ instalando zsh"
+sudo apt install zsh -y
+
+echo "✨ instalando oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "✨ instalando oh-my-posh"
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-chmod +x /usr/local/bin/oh-my-posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
 
 echo "🎨 copiando tema"
 cp -r Modules/omp-themes/default.omp.json ~/.
