909bf7f7a27eb9cd9528b5a0837af0196ccc884f
Cassio Farias Machado <me@cassiofariasmachado.com>
2025-07-26 13:30:39 -0300

    feat: adiciona go, docker-desktop e dotnet

--- install-packages.sh
+++ install-packages.sh
@@ -5,7 +5,6 @@ brew install gh --quiet
 brew install bitwarden --quiet
 brew install git --quiet
 brew install git-delta --quiet
-brew install go --quiet
 brew install gcc --quiet
 brew install bat --quiet
 brew install kubectl --quiet
@@ -14,9 +13,12 @@ brew install k9s --quiet
 brew install oh-my-posh --quiet
 brew install --cask microsoft-edge --quiet
 brew install --cask visual-studio-code --quiet
+brew install go --quiet
+brew install dotnet --quiet
 brew install --cask spotify --quiet
 brew install --cask google-drive --quiet
 brew install --cask displaylink --quiet
 brew install --cask kitty --quiet
 brew install --cask font-meslo-lg-nerd-font --quiet
 brew install --cask font-jetbrains-mono --quiet
+brew install --cask docker-desktop --quiet
